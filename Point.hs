fa79e8c358671f522e20890be2dd20be2f905260
martucrespi <127051398+martucrespi@users.noreply.github.com>
2023-08-26 10:36:05 -0300

    Delete Point.hs

--- Point.hs
+++
@@ -1,9 +0,0 @@
-module Point ( Point, newP, difP)
-   where
-
-data Point = Poi Int Int deriving (Eq, Show)
-
-newP :: Int -> Int -> Point
-newP = Poi
-difP :: Point -> Point -> Float  -- distancia absoluta
-difP (Poi x y) (Poi z w) = sqrt ( fromIntegral (z - x)^2 + fromIntegral (w - y)^2)
