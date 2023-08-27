f8be82800c600bf6151d96decc41e24102c688f5
martucrespi <127051398+martucrespi@users.noreply.github.com>
2023-08-27 19:33:25 -0300

    Update Quality.hs

--- TP 1 funcional/Quality.hs
+++ TP 1 funcional/Quality.hs
@@ -1,4 +1,3 @@
-
 module Quality ( Quality, newQ, capacityQ, delayQ )
    where
 
@@ -12,3 +11,17 @@ capacityQ (Qua s i f) = i
 
 delayQ :: Quality -> Float  -- la demora por unidad de distancia que sucede en las conexiones de este canal
 delayQ (Qua s i f) = f
+
+-- codigo de testeo
+main :: IO ()
+main = do
+    let q1 = newQ "High" 5 10.58
+        q2 = newQ "Low" 1 5.12
+        delay1 = delayQ q1
+        delay2 = delayQ q2
+        capacity1 = capacityQ q1
+        capacity2 = capacityQ q2
+    print delay1
+    print delay2
+    print capacity1
+    print capacity2
