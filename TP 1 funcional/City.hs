4d8231ed167bafd3124f34caa9a2b7ccc16213a9
martucrespi <127051398+martucrespi@users.noreply.github.com>
2023-08-27 19:30:30 -0300

    Update City.hs

--- TP 1 funcional/City.hs
+++ TP 1 funcional/City.hs
@@ -7,7 +7,19 @@ data City = Cit String Point deriving (Eq, Show)
 
 newC :: String -> Point -> City
 newC = Cit
+
 nameC :: City -> String
 nameC (Cit s _) = s
+
 distanceC :: City -> City -> Float
 distanceC (Cit s p) (Cit st po) = difP p po
+
+-- codigo de testeo
+main :: IO ()
+main = do
+    let c1 = newC "Berlin" (newP 1 2)
+        c2 = newC "London" (newP 4 6)
+        distance = distanceC c1 c2
+    print distance
+    print c1
+    print c2
