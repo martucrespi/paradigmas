5569492d57e4ad38438e49092ebd9ce7a792083d
martucrespi <127051398+martucrespi@users.noreply.github.com>
2023-08-27 19:34:50 -0300

    Update Link.hs

--- TP 1 funcional/Link.hs
+++ TP 1 funcional/Link.hs
@@ -1,7 +1,7 @@
-
 module Link ( Link, newL, linksL, connectsL, capacityL, delayL )
    where
 
+import Point 
 import City 
 import Quality
                                               
@@ -15,11 +15,33 @@ connectsL c (Lin c1 c2 _) | c == c1 || c == c2 = True
                            |otherwise = False
 
 linksL :: City -> City -> Link -> Bool -- indica si estas dos ciudades distintas estan conectadas mediante este link
-linksL c1 c2 l | connectsL c1 l == True && connectsL c2 l == True = True
-                  | otherwise = False
+linksL c1 c2 l | connectsL c1 l && connectsL c2 l = True
+               | otherwise = False
 
 capacityL :: Link -> Int
 capacityL (Lin _ _ q) = capacityQ q
 
 delayL :: Link -> Float     -- la demora que sufre una conexion en este canal
 delayL (Lin _ _ q) = delayQ q
+
+-- codigo de testeo
+main :: IO ()
+main = do
+    let c1 = newC "Berlin" (newP 1 2)
+        c2 = newC "London" (newP 4 6)
+        c3 = newC "Buenos Aires" (newP 5 3)
+        q1 = newQ "Quality 1" 2 2.65
+        l1 = newL c1 c2 q1
+        conectado1 = connectsL c1 l1
+        conectado2 = connectsL c3 l1
+        link1 = linksL c1 c2 l1
+        link2 = linksL c2 c3 l1
+        capacidad = capacityL l1
+        delay = delayL l1 
+    print l1
+    print conectado1
+    print conectado2
+    print link1
+    print link2
+    print capacidad
+    print delay
