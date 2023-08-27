90f96d39d4a47447977be2eaf186508055eec954
martucrespi <127051398+martucrespi@users.noreply.github.com>
2023-08-27 19:29:00 -0300

    Update Point.hs

--- TP 1 funcional/Point.hs
+++ TP 1 funcional/Point.hs
@@ -5,5 +5,16 @@ data Point = Poi Int Int deriving (Eq, Show)
 
 newP :: Int -> Int -> Point
 newP = Poi
+
 difP :: Point -> Point -> Float  -- distancia absoluta
 difP (Poi x y) (Poi z w) = sqrt ( fromIntegral (z - x)^2 + fromIntegral (w - y)^2)
+
+-- codigo de testeo
+main :: IO ()
+main = do
+    let p1 = newP 1 2
+        p2 = newP 4 6
+        distance = difP p1 p2
+    print distance
+    print p1
+    print p2
