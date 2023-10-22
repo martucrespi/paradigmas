36c579e73c914ea2840da21da8d78f845e7744b5
martucrespi <127051398+martucrespi@users.noreply.github.com>
2023-10-22 17:38:37 -0300

    Update NemoTests.java

--- TP 3/NemoTests.java
+++ TP 3/NemoTests.java
@@ -3,7 +3,6 @@ package Nemo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
