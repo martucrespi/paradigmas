d9d43cf7592fdfad3a80cb13356a915f258dbe33
PaulaCacchiotti <126919115+PauCacchiotti@users.noreply.github.com>
2023-11-06 10:55:00 -0300

    Add files via upload

---
+++ TP 4/GameTest.java
@@ -0,0 +1,77 @@
+package linea;
+
+import org.junit.Test;
+
+public class GameTest {
+	
+	@Test public void test00StartNewGameA() {
+		
+	}
+	
+	@Test public void test01StartsNewGameB() {
+		
+	}
+	
+	@Test public void test02StartsNewGameC() {
+		
+	}
+	
+	@Test public void test03RedPlays() {
+		
+	}
+	
+	@Test public void test04RedAndBluePlay() {
+		
+	}
+	
+	@Test public void test05NoPlayingOutsideOutOfBoundsWidth() {
+		
+	}
+	
+	@Test public void test06NoPlayingOutsideOutOfBoundsHeight() {
+		
+	}
+	
+	@Test public void test07CannotPlayWhenWonA() {
+		
+	}
+	
+	@Test public void test08CannotPlayWhenWonB() {
+		
+	}
+
+	@Test public void test09CannotPlayWhenWonC() {
+	
+	}
+	
+	@Test public void test10CannotPlayAfterDrawA() {
+		
+	}
+	
+	@Test public void test11CannotPlayAfterDrawB() {
+		
+	}
+
+	@Test public void test12CannotPlayAfterDrawC() {
+	
+	}
+	
+	@Test public void test13RedCannotPlayTwice() {
+		
+	}
+	
+	@Test public void test14BlueCannotPlayTwice() {
+		
+	}
+	
+	
+
+}
+
+// si o si el codigo pregunta que jugada queres hacer o si por ejemplo podes hacerlo llamando a una funcion jugar?
+// hay q si o si seguir la interfaz
+
+
+
+
+
