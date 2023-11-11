bceec57eb752ce5952d04be0289461471054ba56
martucrespi <127051398+martucrespi@users.noreply.github.com>
2023-11-11 18:50:50 -0300

    Add files via upload

---
+++ TP 4/GameType0.java
@@ -0,0 +1,8 @@
+package linea0;
+
+public abstract class GameType0 {
+
+	char key;
+	public abstract char getKey();
+	public abstract boolean finished(Linea0 linea);
+}
---
+++ TP 4/Linea0.java
@@ -0,0 +1,101 @@
+package linea0;
+
+import java.util.ArrayList;
+
+
+public class Linea0 {
+	
+	public ArrayList<ArrayList<String>> board;
+	
+	public int rows;
+	public int columns;
+	public char gametype;
+	private String turn;
+	public ArrayList<GameType0> gameTypes = new ArrayList<GameType0>();
+	
+	public Linea0 (int rows, int columns, char gameType) {
+		this.rows = rows;
+		this.columns = columns;
+		this.gametype = gameType;
+		setTurn("R");
+		
+		this.board = new ArrayList<ArrayList<String>>();
+		for (int i = 0; i < columns; i++) {
+			this.board.add(new ArrayList<String>());
+		}
+	}
+	
+	public GameType0 gameTypeFor(char gtype) {
+		gameTypes.add(new GameTypeA0());
+		gameTypes.add(new GameTypeB0());
+		gameTypes.add(new GameTypeC0());
+   	
+   		return gameTypes.stream()
+   		        .filter(type -> gtype == type.getKey())
+   		        .findFirst()
+   		        .orElse(null);
+   	}
+	
+	public boolean finished() {
+		return this.gameTypeFor(this.gameType()).finished(this);
+	}
+	
+	public void setTurn(String turn) {
+		this.turn = turn;
+	}
+	
+	public int columns() {
+		return this.columns;
+	}
+	
+	public int rows () {
+		return this.rows;
+	}
+	
+	public char gameType() {
+		return this.gametype;
+	}
+	
+	public boolean redPlays() {
+		return turn() == "R";
+	}
+	
+	public boolean bluePlays() {
+		return turn() == "B";
+	}
+	
+	public String turn() {
+		return turn;
+	}
+	
+	
+	public void playRedAt(int column) {
+		if (turn == "R") {
+			if (column > this.columns()) {
+				throw new Error ("Cannot play out of bounds");
+			}else {
+				setTurn("B");
+				int columnIndex = column -1;
+				board.get(columnIndex).add("X");
+			}
+		}else {
+			throw new Error ("Red cannot play twice");
+		}
+	}
+	
+	public void playBlueAt(int column) {
+		if (turn == "B") {
+			if (column > this.columns()) {
+				throw new Error ("Cannot play out of bounds");
+			}else {
+				setTurn("R");
+				int columnIndex = column -1;
+				board.get(columnIndex).add("O");
+			}
+		}else {
+			throw new Error ("Blue cannot play twice");
+		}
+	}
+	
+
+}
